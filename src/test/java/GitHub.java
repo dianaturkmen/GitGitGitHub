c3407b1819caacb03da0c0b2915647dae3786df2
Diana Turkmen <cemile.turkmen@gmail.com>
2022-03-30 20:52:57 +0100

    5.0

--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -3,7 +3,8 @@ public class GitHub {
         System.out.println("GitHub");
                 System.out.println("1. degisiklikGitHub");
 
-                System.out.println("1. degisiklikGitHub");
+                System.out.println("3. degisiklikGitHub");
+                System.out.println("5. degisiklikGitHub");
 
     }
 }
